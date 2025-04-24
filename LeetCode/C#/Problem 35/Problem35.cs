c96b81b82326cac61f3acd320f7cdac328e3ea3a
Christopher Cosgrove <CHRISTOPHER252@bailliegifford.com>
2025-04-24 16:12:52 +0100

    Adding last challenge.

---
+++ LeetCode/C#/Problem 35/Problem35.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Problem_35;
+
+public class Problem35
+{
+    public static int SearchInsertPosition(int[] nums, int target)
+    {
+        var low = 0;
+        var high = nums.Length - 1;
+        var guess = 0;
+        while (low <= high)
+        {
+
+            var midIndex = (low + high) / 2;
+            guess = nums[midIndex];
+
+            if (guess == target)
+            {
+                return midIndex;
+            }
+
+            if (guess < target)
+            {
+                low = midIndex + 1;
+            }
+
+            if (guess > target)
+            {
+                high = midIndex - 1;
+            }
+        }
+
+        return low;
+    }
+}
